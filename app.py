72a13674418a8e9b8334c3c91a47ccaee9da92f4
joelgerge-web <joelgeorge98765@gmail.com>
2023-06-06 13:26:31 +0530

    reduce opening time

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
-    threading.Timer(1.25, open_browser).start()
+    threading.Timer(0.25, open_browser).start()
     app.run(host='0.0.0.0',port = 5000)
